671ae62f1217027b5dca49897abcc3b11fc520ad
刘纪平 <jipings@outlook.com>
2019-02-03 20:43:25 +0800

    update

--- hello.go
+++ hello.go
@@ -2,19 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-   var a int = 4
-   var b int32
-   var c float32
-   var ptr *int
+func main()  {
+	// 定义局部变量
+	var i, j int
 
-   /* 运算符实例 */
-   fmt.Printf("第 1 行 - a 变量类型为 = %T\n", a );
-   fmt.Printf("第 2 行 - b 变量类型为 = %T\n", b );
-   fmt.Printf("第 3 行 - c 变量类型为 = %T\n", c );
-
-   /*  & 和 * 运算符实例 */
-   ptr = &a    /* 'ptr' 包含了 'a' 变量的地址 */
-   fmt.Printf("a 的值为  %d\n", a);
-   fmt.Printf("*ptr 为 %d\n", *ptr);
+	for i = 2; i< 100; i++ {
+		for j = 2; j<= (i/j); j++ {
+			if(i%j == 0) {
+				break; // 如果发现因子，则不是素数
+			}
+		}
+		if(j > (i/j)) {
+			fmt.Printf("%d  是素数\n", i);
+		}
+	}
 }
