415ce24badab8b27c177260b8a208706253b2315
刘纪平 <jipings@outlook.com>
2019-02-06 23:07:57 +0800

    update

--- hello.go
+++ hello.go
@@ -1,21 +1,9 @@
+
 package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y,x
+func main() {
+	var ptr *int
+	fmt.Printf("ptr 的值为 : %x\n", *ptr  )
 }
-
-func main()  {
-	var n [10]int;
-	var i,j int;
-
-	for i = 0; i< 10; i++ {
-		n[i] = i+100
-	}
-
-	for j = 0; j < 10; j++ {
-		fmt.Printf("Element[%d] = %d\n",j, n[j])
-	}
-}
-
