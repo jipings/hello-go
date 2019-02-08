e1444f5564eb37882cecf140b5cdad38e6f61d6e
刘纪平 <jipings@outlook.com>
2019-02-09 00:10:08 +0800

    update

--- go-design/01/server1/main.go
+++ go-design/01/server1/main.go
@@ -6,13 +6,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
-
+var mu sync.Mutex
+var count int
 func main()  {
 	http.HandleFunc("/",handler) // 响应请求调用处理函数
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000",nil))
 }
-
+// 处理程序回显请求的URL的路径部分
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// counter 回显目前为止调用的次数
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
 }
