a8c760789ad49e123e194ed4e109137a1b4160cd
刘纪平 <jipings@outlook.com>
2019-06-04 16:07:45 +0800

    update

---
+++ GoByExample/goroutine/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":",i)
+	}
+}
+
+func main()  {
+
+
+	go f("goroutine")
+	go f("goroutine1")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	f("direct")
+
+	var input string
+	fmt.Scanln(&input)
+
+	fmt.Println("done")
+
+	//for  {
+	//	time.Sleep(time.Millisecond)
+	//}
+}
